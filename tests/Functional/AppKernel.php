6f4179a589cb6e99b24e3886d3d2d6c3aca192bf
Christopher Georg <chris@sky-scripts.de>
2021-10-03 11:44:26 +0200

    fix: deprecations

--- tests/Functional/AppKernel.php
+++ tests/Functional/AppKernel.php
@@ -14,6 +14,7 @@ namespace Fidry\PsyshBundle\Functional;
 use Fidry\PsyshBundle\PsyshBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel;
 
 /**
@@ -32,5 +33,24 @@ class AppKernel extends Kernel
     public function registerContainerConfiguration(LoaderInterface $loader): void
     {
         $loader->load(__DIR__.'/config.yml');
+    }
+
+    # to support different symfony versions we need to handle cofnigs differently
+    protected function configureContainer(ContainerConfigurator $container, LoaderInterface $loader)
+    {
+        # symfony 5.3 https://github.cgs.me/symfony/symfony/blob/5.4/UPGRADE-5.3.md#frameworkbundle
+        $params = [];
+        if (version_compare(\Symfony\Component\HttpKernel\Kernel::VERSION, '5.3.0') >= 0)
+        {
+            $params = ["session" => ['storage_id' => 'session.storage.mock_file']];
+        }
+        else {
+            $params = ["session" => ['storage_factory_id' => 'session.storage.factory.mock_file']];
+        }
+
+        // PHP equivalent of config/packages/framework.yaml
+        $container->extension('framework', [
+            $params
+        ]);
     }
 }
